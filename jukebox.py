a20ff01a97658968715ef59ec52b6cca43fe0399
Armand Vigne-Flett <armandquot@gmail.com>
2020-12-04 11:03:42 +0000

    Update jukebox.py

--- jukebox.py
+++ jukebox.py
@@ -50,6 +50,8 @@ while True:
 
 #LED setup
 leds = gpiozero.LEDBoard(9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26)
+
+#First LED mode
 ledmode1 ={
 while True:
     leds.on()
