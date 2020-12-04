8e5ea14e4eb508a96b3925cb978322617f5d573a
Armand Vigne-Flett <armandquot@gmail.com>
2020-12-04 13:45:53 +0000

    Update jukebox.py
    Setup button for changing LED mode, and organised the LED definition.

--- jukebox.py
+++ jukebox.py
@@ -12,41 +12,6 @@ stopbutton = gpiozero.Button(6)
 shutdownbutton = gpiozero.Button(7)
 #button to change the led mode
 ledbutton = gpiozero.Button(8)
-#setting which mp3 folder to use
-if len(sys.argv) <=1:
-    print('Please specify a folder') #for testing, won't be kept for non-lcd V1
-    sys.exit(1)
-folder = sys.argv[1]
-files = glob.glob(folder+"/*.mp3")
-if len(files) == 0:
-    print('No mp3 files in directory', folder,'..exiting') #for testing, won't be kept for non-lcd V1
-    sys.exit(1)
-
-#vlc player setup
-player = vlc.MediaPlayer()
-#playlist setup
-medialist = vlc.MediaList(files)
-#linking it all
-mlplayer = vlc.MediaListPlayer()
-mlplayer.set_media_player(player)
-mlplayer.set_media_list(medialist)
-#defining the shutdown button
-def shutdown():
-    check_call(['sudo', 'shutdown', '-h', 'now'])
-while True:
-    if playpausebutton.is_pressed:
-        if mlplayer.is_playing():
-            mlplayer.pause() #pauses the music when the playpausebutton is pressed
-        else:
-            mlplayer.play() #plays the music when the playpausebutton is pressed
-    elif stopbutton.is_pressed:
-        mlplayer.stop() #stops the music when the stopbutton is pressed
-    elif trackforwardsbutton.is_pressed:
-        mlplayer.previous() #skips forwards one track when the trackforwardsbutton is pressed
-    elif trackbackbutton.is_pressed:
-        mlplayer.next() #skips backwards one track when the trackbackbutton is pressed
-    elif shutdownbutton.when_pressed:
-        shutdown() #shuts down the pi when the shutdownbutton is pressed
 
 #LED setup
 led1 = gpiozero.PWMLED(9)
@@ -68,6 +33,52 @@ led16 = gpiozero.PWMLED(24)
 led17 = gpiozero.PWMLED(25)
 led18 = gpiozero.PWMLED(26)
 leds = gpiozero.PWMLED(9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26)
+
+#setting which mp3 folder to use
+if len(sys.argv) <=1:
+    print('Please specify a folder') #for testing, won't be kept for non-lcd V1
+    sys.exit(1)
+folder = sys.argv[1]
+files = glob.glob(folder+"/*.mp3")
+if len(files) == 0:
+    print('No mp3 files in directory', folder,'..exiting') #for testing, won't be kept for non-lcd V1
+    sys.exit(1)
+
+#vlc player setup
+player = vlc.MediaPlayer()
+#playlist setup
+medialist = vlc.MediaList(files)
+#linking it all
+mlplayer = vlc.MediaListPlayer()
+mlplayer.set_media_player(player)
+mlplayer.set_media_list(medialist)
+
+#defining the shutdown button
+def shutdown():
+    check_call(['sudo', 'shutdown', '-h', 'now'])
+while True:
+    if playpausebutton.is_pressed:
+        if mlplayer.is_playing():
+            mlplayer.pause() #pauses the music when the playpausebutton is pressed
+        else:
+            mlplayer.play() #plays the music when the playpausebutton is pressed
+    elif stopbutton.is_pressed:
+        mlplayer.stop() #stops the music when the stopbutton is pressed
+    elif trackforwardsbutton.is_pressed:
+        mlplayer.previous() #skips forwards one track when the trackforwardsbutton is pressed
+    elif trackbackbutton.is_pressed:
+        mlplayer.next() #skips backwards one track when the trackbackbutton is pressed
+    elif shutdownbutton.when_pressed:
+        shutdown() #shuts down the pi when the shutdownbutton is pressed
+    elif ledbutton.is_pressed:
+        if ledmode = ledmode1:
+            ledmode = ledmode2
+        elif ledmode = ledmode2:
+            ledmode = ledmode3
+        elif ledmode = ledmode3:
+            ledmode = ledmode4
+        elif ledmode = ledmode4:
+            ledmode = ledmode1
 
 #First LED mode is flash on off on off ...
 ledmode1 = {
